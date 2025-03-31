b698d51c5fbb91ea6a7edf6c5accbef371597ff5
SzymonKamiński <szymon.kaminkk@gmail.com>
2025-03-31 09:08:00 +0200

    feat: added in App.tsx test curl from server

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  useEffect(() => {
+    fetch('/api/text')
+      .then(response => response.text())
+      .then(data => setText(data));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          {text && <p>Server response: {text}</p>}
+
         <a
           className="App-link"
           href="https://reactjs.org"
