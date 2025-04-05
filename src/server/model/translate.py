486db0b78706788c3d0f7ff03e503923e304875c
Kerciu <kerciuuu@gmail.com>
2025-04-05 19:38:44 +0200

    added methods of translator as static

--- src/server/model/translate.py
+++ src/server/model/translate.py
@@ -6,12 +6,13 @@ class Translator:
     def __init__(self):
         pass
 
+    @staticmethod
     def detect_language(self, text: str) -> str:
         try:
             return detect(text)
         except LangDetectException as e:
             raise ValueError(f"Language detection failed: {e}") from e
 
+    @staticmethod
     def translate(self, text: str, target_language: str) -> str:
-
-        return f"Bonjour"
+        return "Bonjour"
