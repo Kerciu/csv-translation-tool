ae54be4bd193cc25ed3ae63bca4fbe31b7971de7
Kerciu <kerciuuu@gmail.com>
2025-04-05 19:48:32 +0200

    cached models in for optimization, added more test cases

--- src/server/model/test/test_translate.py
+++ src/server/model/test/test_translate.py
@@ -3,10 +3,22 @@ from src.server.model.translate import Translator
 translator = Translator()
 
 
-def test_translator_language_detect():
+def test_translator_language_detect_en():
     text = "Hello, how are you?"
     detected_language = translator.detect_language(text)
     assert detected_language == "en", f"Expected 'en', got '{detected_language}'"
+
+
+def test_translator_language_detect_de():
+    text = "Guten tag!"
+    detected_language = translator.detect_language(text)
+    assert detected_language == "de", f"Expected 'de', got '{detected_language}'"
+
+
+def test_translator_language_detect_pl():
+    text = "Cześć, jak się masz?"
+    detected_language = translator.detect_language(text)
+    assert detected_language == "pl", f"Expected 'pl', got '{detected_language}'"
 
 
 def test_translator_translate():
--- src/server/model/translate.py
+++ src/server/model/translate.py
@@ -1,21 +1,32 @@
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
+from transformers import MarianMTModel, MarianTokenizer
+from typing import Set
 
 
 class Translator:
     def __init__(self):
-        pass
+        self.cached_models: Set[MarianMTModel] = {}
 
     def _get_model_name(self, src_lang: str, tgt_lang: str) -> str:
         return f"Helsinki-NLP/opus-mt-{src_lang}-{tgt_lang}"
 
+    def _load_model(self, src_lang: str, tgt_lang: str) -> MarianMTModel:
+        model_name = self._get_model_name(src_lang, tgt_lang)
+
+        if model_name not in self.cached_models:
+            tokenizer = MarianTokenizer.from_pretrained(model_name)
+            model = MarianMTModel.from_pretrained(model_name)
+            self.cached_models[model_name] = (tokenizer, model)
+
+        return self.cached_models[model_name]
+
     @staticmethod
-    def detect_language(self, text: str) -> str:
+    def detect_language(text: str) -> str:
         try:
             return detect(text)
         except LangDetectException as e:
             raise ValueError(f"Language detection failed: {e}") from e
 
-    @staticmethod
     def translate(self, text: str, target_language: str) -> str:
         return "Bonjour"
