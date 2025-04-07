a7cffa648169f84a37006e49288c2674488f088d
Kerciu <Kerciuuu@gmail.com>
2025-04-07 23:08:30 +0200

    created auth provider that supplies hook with methods for authentication that will be expanded in the future

--- src/client/src/src/context/AuthContext.tsx
+++ src/client/src/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import type React from "react"
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
 
 interface AuthContextProps {
     user: any | null
@@ -11,7 +11,26 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
-const useAuth = () => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<any>(null);
+
+    const login = async (email: string, password: string) => {}
+    const register = async (username: string, email: string, password: string) => {}
+    const logout = () => {}
+
+    const value: AuthContextProps = {
+        user,
+        isLoading,
+        login,
+        register,
+        logout
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) throw new Error("Error: useAuth must be used within AuthContext");
     return context;
