16228c990e8ae7d4b911c1ab3f71571dd1d3fcfc
Kerciu <Kerciuuu@gmail.com>
2025-04-07 23:02:38 +0200

    created auth context and hook for it

--- src/client/src/src/context/AuthContext.tsx
+++ src/client/src/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import type React from "react"
+import { createContext, useContext } from "react"
 
 interface AuthContextProps {
     user: any | null
@@ -6,4 +7,12 @@ interface AuthContextProps {
     login: (email: string, password: string) => Promise<void>
     register: (username: string, email: string, password: string) => Promise<void>
     logout: () => void
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("Error: useAuth must be used within AuthContext");
+    return context;
 }
