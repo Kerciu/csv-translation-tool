c32aede24069e9e240ca8e573d885b90b4859ba7
Kerciu <Kerciuuu@gmail.com>
2025-04-08 13:35:20 +0200

    added styles for the footer, creators are placed on the left and text are foregrounded

--- src/client/components/footer.tsx
+++ src/client/components/footer.tsx
@@ -5,15 +5,15 @@ const Footer = () => {
   return (
     <footer className='border-t py-6 md:py-0'>
       <div className='container mx-auto px-4'>
-        <div>
-          <p>
+        <div className='flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row'>
+          <p className='text-center text-sm text-muted-foreground md:text-left'>
             CSV Translation Tool. All rights reserved.
           </p>
 
-          <div>
-            <div>
-              <span>Created by:</span>
-              <div>
+          <div className='flex flex-col items-center gap-4 md:flex-row'>
+            <div className='flex items-center gap-2'>
+              <span className='text-sm text-muted-foreground'>Created by:</span>
+              <div className='flex flex-col md:flex-row md:gap-4'>
                 <CreatorInfo fullName="Kacper Górski" githubLink="8.8.8.8" linkedinLink="8.8.8.8"/>
                 <CreatorInfo fullName="Szymon Kamiński" githubLink="8.8.8.8" linkedinLink="8.8.8.8"/>
               </div>
