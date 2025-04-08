977a244cd6bbaeb67d0974d5587b1b1479868c74
Kerciu <Kerciuuu@gmail.com>
2025-04-08 13:18:58 +0200

    added footer component to the home page

--- src/client/app/page.tsx
+++ src/client/app/page.tsx
@@ -1,11 +1,16 @@
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar/>
-      <Button>123c</Button>
+      <Navbar />
+
+      <main className="flex-1">
+        Contents
+      </main>
+
+      <Footer/>
     </div>
   )
 }
