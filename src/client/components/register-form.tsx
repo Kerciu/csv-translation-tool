f617f8b5b233404cd7d6b7078a78a9c17a46763f
Kerciu <Kerciuuu@gmail.com>
2025-04-09 17:28:09 +0200

    made register form skeleton

--- src/client/components/register-form.tsx
+++ src/client/components/register-form.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import AuthButton from './auth-button'
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
+    <>
+        <div>
+            <div>
+                <Label>
+                    Username
+                </Label>
+                <Input
+                />
+            </div>
+            <div>
+                <Label>
+                    Email
+                </Label>
+                <Input
+                />
+            </div>
+            <div>
+                <Label>
+                    Password
+                </Label>
+                <Input
+                />
+            </div>
+
+            <AuthButton message='Register with Email'/>
+        </div>
+    </>
   )
 }
 
