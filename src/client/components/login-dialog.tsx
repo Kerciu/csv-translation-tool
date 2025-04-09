16261b826358b5a701415650cba7444faba776f9
Kerciu <Kerciuuu@gmail.com>
2025-04-09 16:54:28 +0200

    created a dialog header and made some methods for allowing to load the dialog

--- src/client/components/login-dialog.tsx
+++ src/client/components/login-dialog.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Dialog, DialogHeader } from '@/components/ui/dialog'
+import { DialogContent, DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
 
 interface LoginDialogProps {
     isOpen: boolean
     onOpenChange: (open: boolean) => void
 }
 
-const LoginDialog = ({isOpen, onOpenChange}) => {
-  return (
-    <div>LoginDialog</div>
-  )
-}
+const LoginDialog = ({isOpen, onOpenChange}: LoginDialogProps) => {
+
+    const [isLoading, setIsLoading] = useEffect(false);
+    const [isSubmitting, setIsSubmitting] = useEffect(false);
+
+    const isProcessing = isLoading || isSubmitting;
+
+    return (
+        <Dialog
+            open={isOpen}
+            onOpenChange={(newOpen) => {
+                if (!isProcessing) 
+                    onOpenChange(newOpen)
+                }
+            }
+        >
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Welcome to the CSV Translator</DialogTitle>
+                    <DialogDescription>Sign in to access the full translation features</DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+    }
 
 export default LoginDialog
