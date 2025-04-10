342836227adef81f57151e0a58eaaf79bda19ede
Kerciu <Kerciuuu@gmail.com>
2025-04-10 13:46:19 +0200

    created skeleton for csv uploader, file input and button to upload the file

--- src/client/components/csv-uploader.tsx
+++ src/client/components/csv-uploader.tsx
@@ -1,8 +1,22 @@
+import { FileSpreadsheet, Upload } from 'lucide-react'
 import React from 'react'
+import { Button } from './ui/button'
 
 const CSVUploader = () => {
   return (
-    <div>CSVUploader</div>
+    <div>
+        <input />
+
+        <FileSpreadsheet/>
+
+        <h3>Upload your CSV File</h3>
+        <p>Drag and drop your file here, or click the button below</p>
+
+        <Button>
+            <Upload />
+            Select your CSV File
+        </Button>
+    </div>
   )
 }
 
