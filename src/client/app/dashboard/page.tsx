7c0710e3ce059f3e39bf9ec36e04ac662de82386
Kerciu <Kerciuuu@gmail.com>
2025-04-10 13:37:28 +0200

    created dashboard component that will display file upload, and main logic of the app

---
+++ src/client/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+
+      <main className="flex-1">
+        CSV Uploader will be placed here TODO
+      </main>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default Dashboard
