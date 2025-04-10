9fc238ebf170693327a25cd42cd07e8c2d45674a
Kerciu <Kerciuuu@gmail.com>
2025-04-10 18:49:35 +0200

    created empty functions to handle dragging in csv uploader

--- src/client/components/csv-uploader.tsx
+++ src/client/components/csv-uploader.tsx
@@ -1,5 +1,5 @@
 import { FileSpreadsheet, Upload } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 
 interface CSVUploaderProps {
@@ -7,25 +7,49 @@ interface CSVUploaderProps {
 }
 
 const CSVUploader = ({ onFileUpload }: CSVUploaderProps) => {
-  return (
-    <div className='border-2 border-dashed rounded-lg p-10 text-center'>
-        <input
-            type='file'
-            accept='.csv'
-            className='hidden'
-        />
 
-        <FileSpreadsheet className='mx-auto h-12 w-12 text-muted-foreground mb-4'/>
+    const [isDragging, setDragging] = useState<boolean>(false);
 
-        <h3 className='text-lg font-medium mb-2'>Upload your CSV File</h3>
-        <p className='text-muted-foreground mb-6'>Drag and drop your file here, or click the button below</p>
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDragging(true);
+        /* ... */
+    }
 
-        <Button className='gap-2'>
-            <Upload className='h-4 w-4'/>
-            Select your CSV File
-        </Button>
-    </div>
-  )
+    const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDragging(false);
+        /* ... */
+    }
+
+    const handleDragDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDragging(false);
+        /* ... */
+    }
+
+    return (
+        <div className='border-2 border-dashed rounded-lg p-10 text-center'>
+            <input
+                type='file'
+                accept='.csv'
+                className='hidden'
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDragDrop}
+            />
+
+            <FileSpreadsheet className='mx-auto h-12 w-12 text-muted-foreground mb-4'/>
+
+            <h3 className='text-lg font-medium mb-2'>Upload your CSV File</h3>
+            <p className='text-muted-foreground mb-6'>Drag and drop your file here, or click the button below</p>
+
+            <Button className='gap-2'>
+                <Upload className='h-4 w-4'/>
+                Select your CSV File
+            </Button>
+        </div>
+    )
 }
 
 export default CSVUploader
