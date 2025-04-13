78b6835bf416ccf53124cf9fe5f2b4d12a87b4f0
Kerciu <Kerciuuu@gmail.com>
2025-04-13 22:42:19 +0200

    added loading icon when loading from auth or data load is not done yet

--- src/client/app/dashboard/page.tsx
+++ src/client/app/dashboard/page.tsx
@@ -4,23 +4,28 @@ import CSVTable from '@/components/csv-table';
 import CSVUploader from '@/components/csv-uploader'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import { useAuth } from '@/hooks/use-auth';
+import { Loader2 } from 'lucide-react';
 import React, { useState } from 'react'
 
 
 
 const Dashboard = () => {
 
-    const [fileUploaded, setFileUploaded] = useState<boolean>(true);
+    const [fileUploaded, setFileUploaded] = useState<boolean>(false);
+    
+    const [isLoading, setLoading] = useState(true);
+    const { user, isLoading: authLoading } = useAuth();
 
     const handleFileUpload = () => {}
 
     /* DEBUG: Mock Data */
     const headers = ["Name", "Age", "Country", "Email"]
     const data = [
-    ["John Doe", "30", "USA", "john.doe@example.com"],
-    ["Jane Smith", "25", "Canada", "jane.smith@example.com"],
-    ["Sam Brown", "28", "UK", "sam.brown@example.com"],
-    ["Emily White", "22", "Australia", "emily.white@example.com"]
+        ["John Doe", "30", "USA", "john.doe@example.com"],
+        ["Jane Smith", "25", "Canada", "jane.smith@example.com"],
+        ["Sam Brown", "28", "UK", "sam.brown@example.com"],
+        ["Emily White", "22", "Australia", "emily.white@example.com"]
     ]
 
     const selectedColumns = ["Name", "Age", "Email"]
@@ -29,6 +34,21 @@ const Dashboard = () => {
         console.log(`Updated cell at row ${rowIndex + 1}, column ${colIndex}: ${value}`)
     }
     /* DEBUG */
+
+    if (isLoading || authLoading)
+    {
+        return <div className="flex min-h-screen flex-col">
+            <Navbar />
+            <main>
+                <div>
+                    <Loader2/>
+                    <p>Loading your dashboard...</p>
+                </div>
+            </main>
+
+            <Footer/>
+        </div>
+    }
 
     return (
         <div className="flex min-h-screen flex-col">
