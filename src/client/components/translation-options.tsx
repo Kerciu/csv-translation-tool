5251e5fc0b971279966178021091119afc53b411
Kerciu <Kerciuuu@gmail.com>
2025-04-13 23:27:45 +0200

    integrated data and language translation options into translation options component

--- src/client/components/translation-options.tsx
+++ src/client/components/translation-options.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import DataTranslationOption from './column-translation-option'
+import LanguageTranslationOption from './language-translation-option'
 
 const TranslationOptions = () => {
   return (
-    <div>TranslationOptions</div>
+    <div className='grid md:grid-cols-2 gap-4'>
+        <DataTranslationOption/>
+        <LanguageTranslationOption/>
+    </div>
   )
 }
 
