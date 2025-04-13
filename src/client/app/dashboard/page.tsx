76029fed5301717d4394e329119ad58cd790a0e9
Kerciu <Kerciuuu@gmail.com>
2025-04-13 22:50:37 +0200

    fixed positioning of the uploader

--- src/client/app/dashboard/page.tsx
+++ src/client/app/dashboard/page.tsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
 
     const [fileUploaded, setFileUploaded] = useState<boolean>(false);
     
-    const [isLoading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(false);
     const { user, isLoading: authLoading } = useAuth();
 
     const handleFileUpload = () => {}
@@ -54,7 +54,7 @@ const Dashboard = () => {
         <div className="flex min-h-screen flex-col">
         <Navbar />
 
-        <main className="flex-1">
+        <main className="container mx-auto py-8 px-4 flex-1">
             {fileUploaded ?
                 <CSVTable
                     headers={headers} 
