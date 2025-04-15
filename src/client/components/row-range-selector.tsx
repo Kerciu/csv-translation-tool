c4a063ac44c18ae1af2868d13531ce98a5d79df7
Kerciu <Kerciuuu@gmail.com>
2025-04-15 17:38:55 +0200

    made row selection from start to end a label and input component

--- src/client/components/row-range-selector.tsx
+++ src/client/components/row-range-selector.tsx
@@ -1,33 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
 
 const RowRangeSelector = () => {
-  return (
-    <Card>
-        <CardContent>
-            <div>
+
+    const [startRow, setStartRow] = useState<number>(1);
+    const [endRow, setEndRow] = useState<number>(1);
+        
+    return (
+        <Card>
+            <CardContent>
                 <div>
                     <div>
-                        Start Row
+                        <div>
+                            <Label  htmlFor='start-row'>
+                                Start Row
+                            </Label>
+                            <Input
+                                id='start-row'
+                                type='number'
+                                value={startRow}
+                            />
+                        </div>
+
+                        <div>
+                            <Label  htmlFor='end-row'>
+                                End Row
+                            </Label>
+                            <Input
+                                id='end-row'
+                                type='number'
+                                value={endRow}
+                            />
+                        </div>
                     </div>
 
                     <div>
-                        End Row
+                        <div> Selected # of # rows</div>
+
+                        <Button>
+                            Select All Rows
+                        </Button>
                     </div>
                 </div>
-
-                <div>
-                    <div> Selected # of # rows</div>
-
-                    <Button>
-                        Select All Rows
-                    </Button>
-                </div>
-            </div>
-        </CardContent>
-    </Card>
-  )
+            </CardContent>
+        </Card>
+    )
 }
 
 export default RowRangeSelector
