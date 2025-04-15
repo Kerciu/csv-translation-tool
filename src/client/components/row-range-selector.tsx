cb4c3423b5a395c1e6e6798c3e843ec7668c2173
Kerciu <Kerciuuu@gmail.com>
2025-04-15 17:32:44 +0200

    implemented basic structure of row range selector

--- src/client/components/row-range-selector.tsx
+++ src/client/components/row-range-selector.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Card, CardContent } from './ui/card'
+import { Button } from './ui/button'
 
 const RowRangeSelector = () => {
   return (
-    <div>RowRangeSelector</div>
+    <Card>
+        <CardContent>
+            <div>
+                <div>
+                    <div>
+                        Start Row
+                    </div>
+
+                    <div>
+                        End Row
+                    </div>
+                </div>
+
+                <div>
+                    <div> Selected # of # rows</div>
+
+                    <Button>
+                        Select All Rows
+                    </Button>
+                </div>
+            </div>
+        </CardContent>
+    </Card>
   )
 }
 
