a318987b532f74ab80acf0cd0e4aa627649b3edc
Kerciu <Kerciuuu@gmail.com>
2025-04-17 12:31:19 +0200

    created dialog header structure for upload confirmation dialog

--- src/client/components/upload-confirmation-dialog.tsx
+++ src/client/components/upload-confirmation-dialog.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Dialog } from './ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from './ui/dialog'
+import { AlertTriangle } from 'lucide-react'
 
 interface UploadConfirmationDialogProps {
   open: boolean
@@ -10,7 +11,22 @@ interface UploadConfirmationDialogProps {
 const UploadConfirmationDialog = ({ open, onOpenChange, onConfirm }: UploadConfirmationDialogProps) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
 
+        <DialogHeader>
+            <AlertTriangle className='w-5 h-5'/>
+            <span>Upload New File</span>
+          <DialogTitle>
+
+          </DialogTitle>
+
+          <DialogDescription>
+
+          </DialogDescription>
+
+        </DialogHeader>
+
+      </DialogContent>
     </Dialog>
   )
 }
