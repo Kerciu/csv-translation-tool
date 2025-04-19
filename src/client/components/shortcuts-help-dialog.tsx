d640ea7edaadae4d1c6ef67b30b08edeb871d7da
Kerciu <kerciuuu@gmail.com>
2025-04-19 22:33:52 +0200

    added dialog content section, described shortcuts help dialog

--- src/client/components/shortcuts-help-dialog.tsx
+++ src/client/components/shortcuts-help-dialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dialog, DialogTrigger } from './ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button'
 
 const ShortcutsHelpDialog = () => {
@@ -8,6 +8,18 @@ const ShortcutsHelpDialog = () => {
         <DialogTrigger asChild>
             <Button>Keyboard shortcuts</Button>
         </DialogTrigger>
+
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>
+                    Keyboard Shortcuts
+                </DialogTitle>
+
+                <DialogDescription>
+                    Use these keyboard shortcuts to work more efficiently with the CSV translator.
+                </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
     </Dialog>
   )
 }
