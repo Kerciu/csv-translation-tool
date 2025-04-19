ff2c9be7d3c4a858a2a9b0c0a0ba1b8681c7734b
Kerciu <kerciuuu@gmail.com>
2025-04-19 22:26:49 +0200

    moved languages data structure with supported languages to separate folder

---
+++ src/client/data/languages.ts
@@ -0,0 +1,19 @@
+type Language = {
+    value: string
+    label: string
+}
+
+const languages: Language[] = [
+    { value: "es", label: "Spanish" },
+    { value: "fr", label: "French" },
+    { value: "de", label: "German" },
+    { value: "it", label: "Italian" },
+    { value: "pt", label: "Portuguese" },
+    { value: "ru", label: "Russian" },
+    { value: "zh", label: "Chinese" },
+    { value: "ja", label: "Japanese" },
+    { value: "ko", label: "Korean" },
+    { value: "ar", label: "Arabic" },
+]
+
+export default languages;
