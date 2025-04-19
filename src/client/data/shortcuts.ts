02d526ffba651d29e3037bd16710aee5dba3f0f1
Kerciu <kerciuuu@gmail.com>
2025-04-19 23:48:51 +0200

    updated shortcuts for row selection actions and descriptions

--- src/client/data/shortcuts.ts
+++ src/client/data/shortcuts.ts
@@ -6,10 +6,25 @@ type Shortcut = {
 
 const shortcuts: Shortcut[] = [
     {
+        action: "Select a single row",
+        shortcut: "Click on row #",
+        description: "Click on the row number to select a single row",
+      },
+      {
         action: "Select a range of rows",
         shortcut: "Shift + Click",
-        description: "Click on a row number, then Shift+Click on another row to select all rows in between."
-    }
+        description: "Click on a row number, then Shift+Click on another row to select all rows in between",
+      },
+      {
+        action: "Select/deselect multiple rows",
+        shortcut: "Ctrl/Cmd + Click",
+        description: "Hold Ctrl (Windows) or Cmd (Mac) and click on row numbers to select or deselect multiple rows",
+      },
+      {
+        action: "Deselect all rows",
+        shortcut: "Escape",
+        description: "Press Escape to clear all row selections",
+      },
 ]
 
 export default shortcuts;
