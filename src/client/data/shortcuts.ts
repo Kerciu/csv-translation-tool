a9975ead56cce9d181e1ec1c30e72418ab2fcde4
Kerciu <kerciuuu@gmail.com>
2025-04-19 22:29:10 +0200

    feat: add shortcuts data structure for row selection

---
+++ src/client/data/shortcuts.ts
@@ -0,0 +1,13 @@
+type Shortcut = {
+    action: string;
+    shortcut: string;
+    description: string;
+}
+
+const shortcuts: Shortcut[] = [
+    {
+        action: "Select a range of rows",
+        shortcut: "Shift + Click",
+        description: "Click on a row number, then Shift+Click on another row to select all rows in between."
+    }
+]
