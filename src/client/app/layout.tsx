8440da4fed4b33c6e37b342facfffcf3a2be2d07
Kerciu <kerciuuu@gmail.com>
2025-04-19 22:21:22 +0200

    fix: fixed hydration error with system theme

--- src/client/app/layout.tsx
+++ src/client/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <AuthProvider>{children}</AuthProvider>
           <ClientToaster />
         </ThemeProvider>
