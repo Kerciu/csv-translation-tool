f05cf8417c7cd5e1cdaab5050480a4e659d091cd
Kerciu <kerciuuu@gmail.com>
2025-04-20 17:43:42 +0200

    feat: add English language option to languages list

--- src/client/data/languages.ts
+++ src/client/data/languages.ts
@@ -4,6 +4,7 @@ type Language = {
 }
 
 const languages: Language[] = [
+    { value: "en", label: "English"},
     { value: "es", label: "Spanish" },
     { value: "fr", label: "French" },
     { value: "de", label: "German" },
