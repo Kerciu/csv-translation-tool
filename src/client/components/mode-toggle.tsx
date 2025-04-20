610ce113168bf651bfc816ae03c48effb7b7f7be
Kerciu <kerciuuu@gmail.com>
2025-04-20 18:08:38 +0200

    made the whole mode toggle component as a button that will toggle light and dark theme

--- src/client/components/mode-toggle.tsx
+++ src/client/components/mode-toggle.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { cn } from "@/lib/utils"
 
 export function ModeToggle() {
 
@@ -25,26 +26,27 @@ export function ModeToggle() {
 
     const Icon = themeIcons[theme as keyof typeof themeIcons] || Sun;
 
+    const handleClick = () => {}
+
     return (
-        <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-        </DropdownMenu>
+        <Button
+        variant="outline"
+        size="icon"
+        onClick={handleClick}
+        aria-label="Toggle theme"
+        >
+        <div className="relative h-[1.2rem] w-[1.2rem]">
+            <Sun className={cn(
+            "absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all",
+            theme !== "light" && "-rotate-90 scale-0",
+            isAnimating && "animate-pulse"
+            )} />
+            <Moon className={cn(
+            "absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all",
+            theme === "dark" && "rotate-0 scale-100",
+            isAnimating && "animate-pulse"
+            )} />
+        </div>
+        </Button>
     )
 }
