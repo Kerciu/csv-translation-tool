6a1455c66845c088f8718e876b41c7f47888cc84
Kerciu <Kerciuuu@gmail.com>
2025-04-23 19:14:32 +0200

    created grid columns layout for csv preview steps

--- src/client/components/landing/csv-preview-steps.tsx
+++ src/client/components/landing/csv-preview-steps.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 
 const CSVPreviewSteps = () => {
   return (
-    <div>
-        <div>
+    <div className='grid grid-cols-3 gap-2 text-center text-sm'>
+        <div className='flex flex-col items-center'>
           <div>
             <Upload className='h-4 w-4 text-primary'/>
           </div>
           <span>Upload</span>
         </div>
 
-        <div>
+        <div className='flex flex-col items-center'>
           <div>
             <Languages className='h-4 w-4 text-primary'/>
           </div>
           <span>Translate</span>
         </div>
 
-        <div>
+        <div className='flex flex-col items-center'>
           <div>
             <Download className='h-4 w-4 text-primary'/>
           </div>
