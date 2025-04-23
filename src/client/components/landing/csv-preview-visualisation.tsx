94cdb57c85f1bc2ec71eb3ceb81c0b5e85618d17
Kerciu <Kerciuuu@gmail.com>
2025-04-23 19:02:22 +0200

    created beautiful csv preview with simple data

--- src/client/components/landing/csv-preview-visualisation.tsx
+++ src/client/components/landing/csv-preview-visualisation.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const CSVPreviewVisualisation = () => {
   return (
-    <div>
+    <div className='border rounded-md overflow-hidden mb-4'>
         <div className='grid grid-cols-3 text-xs'>
             <div className='font-medium p-2 bg-muted'>Name</div>
             <div className='font-medium p-2 bg-muted'>Description</div>
             <div className='font-medium p-2 bg-muted'>Category</div>
 
-            <div></div>
+            <div className='p-2 border-t bg-primary/5'>Schmitterling</div>
+            <div className='p-2 border-t'>A bug that flies</div>
+            <div className='p-2 border-t'>Animal</div>
+
+            <div className='p-2 border-t'>Computer Science</div>
+            <div className='p-2 border-t bg-primary/5'>Dziedzina nauki</div>
+            <div className='p-2 border-t'>Studies</div>
         </div>
     </div>
   )
