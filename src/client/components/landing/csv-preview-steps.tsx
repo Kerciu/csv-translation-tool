85a6144aff6d47b28519d28fee9f2b19c9e6301f
Kerciu <Kerciuuu@gmail.com>
2025-04-23 19:13:23 +0200

    added upload, translate and download icons for csv preview steps

--- src/client/components/landing/csv-preview-steps.tsx
+++ src/client/components/landing/csv-preview-steps.tsx
@@ -1,9 +1,29 @@
+import { Download, Languages, Upload } from 'lucide-react'
 import React from 'react'
 
 const CSVPreviewSteps = () => {
   return (
     <div>
-        Steps
+        <div>
+          <div>
+            <Upload className='h-4 w-4 text-primary'/>
+          </div>
+          <span>Upload</span>
+        </div>
+
+        <div>
+          <div>
+            <Languages className='h-4 w-4 text-primary'/>
+          </div>
+          <span>Translate</span>
+        </div>
+
+        <div>
+          <div>
+            <Download className='h-4 w-4 text-primary'/>
+          </div>
+          <span>Download</span>
+        </div>
     </div>
   )
 }
