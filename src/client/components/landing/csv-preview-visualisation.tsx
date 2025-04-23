a811653a9db81669a0aa69c3a611f0593e23b715
Kerciu <Kerciuuu@gmail.com>
2025-04-23 19:21:38 +0200

    included id's in csv preview

--- src/client/components/landing/csv-preview-visualisation.tsx
+++ src/client/components/landing/csv-preview-visualisation.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 const CSVPreviewVisualisation = () => {
   return (
     <div className='border rounded-md overflow-hidden mb-4'>
-        <div className='grid grid-cols-3 text-xs'>
+        <div className='grid grid-cols-[0.25fr,1fr,1fr,1fr] text-xs'>
+            <div className='font-medium p-2 bg-muted'>#</div>
             <div className='font-medium p-2 bg-muted'>Name</div>
             <div className='font-medium p-2 bg-muted'>Description</div>
             <div className='font-medium p-2 bg-muted'>Category</div>
 
+            <div className='p-2 border-t'>1</div>
             <div className='p-2 border-t bg-primary/5'>Schmitterling</div>
             <div className='p-2 border-t'>A bug that flies</div>
             <div className='p-2 border-t'>Animal</div>
 
+            <div className='p-2 border-t'>2</div>
             <div className='p-2 border-t'>Computer Science</div>
             <div className='p-2 border-t bg-primary/5'>Dziedzina nauki</div>
             <div className='p-2 border-t'>Studies</div>
