be76895d6784c60836c9f9dba4db1be523788134
Kerciu <Kerciuuu@gmail.com>
2025-04-23 18:58:57 +0200

    implemented headings for csv preview

--- src/client/components/landing/csv-preview-visualisation.tsx
+++ src/client/components/landing/csv-preview-visualisation.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const CSVPreviewVisualisation = () => {
   return (
-    <div>CSVPreviewVisualisation</div>
+    <div>
+        <div className='grid grid-cols-3 text-xs'>
+            <div className='font-medium p-2 bg-muted'>Name</div>
+            <div className='font-medium p-2 bg-muted'>Description</div>
+            <div className='font-medium p-2 bg-muted'>Category</div>
+
+            <div></div>
+        </div>
+    </div>
   )
 }
 
