d64b98d1cba6202754449650cfe7c0f5bad08e5e
Kerciu <Kerciuuu@gmail.com>
2025-04-23 18:35:17 +0200

    structured hero section, added left side with headings and paragraphs

--- src/client/components/landing/hero-section.tsx
+++ src/client/components/landing/hero-section.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Badge } from '../ui/badge'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section>
+        <div>
+            <div>
+                <div>
+                    <Badge variant='outline'>
+                        AI-Powered Translator
+                    </Badge>
+                    <h1>
+                        CSV Translations <span>Made Simple</span>
+                    </h1>
+                    <p>
+                        Translate your CSV files with ease. Choose columns & rows, select languages and edit translations manually.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
