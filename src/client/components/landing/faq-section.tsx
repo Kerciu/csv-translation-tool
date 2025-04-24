c43f959f426e5def09d112011427ce601db019e8
Kerciu <Kerciuuu@gmail.com>
2025-04-24 23:13:44 +0200

    centered faq section and  improved styling

--- src/client/components/landing/faq-section.tsx
+++ src/client/components/landing/faq-section.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const FAQSection = () => {
   return (
-    <section>
-      <div>
-        <div>
+    <section className='py-12 bg-background'>
+      <div className='container px-4 mx-auto'>
+        <div className='text-center mb-8'>
           <h2>Frequently Asked Questions</h2>
           <p>Learn more about our CSV translation tool</p>
         </div>
