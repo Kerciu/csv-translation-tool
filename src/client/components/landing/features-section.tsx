39b604df277c2d881c449b447645c80a5a347828
Kerciu <Kerciuuu@gmail.com>
2025-04-24 22:49:28 +0200

    created skeleton for features section, added headings and mapped through features array to render them

--- src/client/components/landing/features-section.tsx
+++ src/client/components/landing/features-section.tsx
@@ -1,8 +1,38 @@
+import features from '@/data/features'
 import React from 'react'
 
 const FeaturesSection = () => {
   return (
-    <div>FeaturesSection</div>
+    <section>
+      <div>
+        <div>
+          <div>
+            <div>
+              Features
+            </div>
+            <h2>
+              Everything You Need for CSV Translation
+            </h2>
+            <p>
+              Our powerful translation tool provides all the features you need to translate your CSV files quickly and accurately.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          {features.map((feat, idx) => (
+            <div>
+              <div>
+                <feat.icon />
+              </div>
+
+              <h3>{feat.title}</h3>
+              <p>{feat.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
