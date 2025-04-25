b641ccecaab1fd89400c37bfd34caff63016684a
Kerciu <Kerciuuu@gmail.com>
2025-04-25 11:53:54 +0200

    implemented dialog heading in cell translation dialog

--- src/client/components/cell-translation-dialog.tsx
+++ src/client/components/cell-translation-dialog.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from './ui/dialog'
 
 interface CellTranslationDialogProps {
     open: boolean
     onOpenChange: (open: boolean) => void
+    rowIdx: number
+    columnName: string
 }
 
-const CellTranslationDialog = () => {
-  return (
-    <div>CellTranslationDialog</div>
-  )
+const CellTranslationDialog = ({
+    open,
+    onOpenChange,
+    rowIdx,
+    columnName
+    }: CellTranslationDialogProps) => {
+
+    return (
+        <Dialog>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Cell Translation - Row {rowIdx + 1}, {columnName}
+                    </DialogTitle>
+                    <DialogDescription>
+                        Review and edit the translation for this cell.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default CellTranslationDialog
