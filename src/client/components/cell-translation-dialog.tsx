40a11660ed7ef81b57828a0508dbd160b1b60109
Kerciu <Kerciuuu@gmail.com>
2025-04-25 11:49:49 +0200

    created cell translation dialog component

---
+++ src/client/components/cell-translation-dialog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface CellTranslationDialogProps {
+    open: boolean
+    onOpenChange: (open: boolean) => void
+}
+
+const CellTranslationDialog = () => {
+  return (
+    <div>CellTranslationDialog</div>
+  )
+}
+
+export default CellTranslationDialog
