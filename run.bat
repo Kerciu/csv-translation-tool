284444ec1db0d4028a4487b8b71f061888432c9d
SzymonKamiński <szymon.kaminkk@gmail.com>
2025-04-26 20:50:19 +0200

    quickfix: rust python wrapper works corectly

--- run.bat
+++ run.bat
@@ -1,16 +1,10 @@
 @echo off
 echo 🚀 Starting server...
 
-NET FILE >nul 2>&1 || (
-    echo Requesting administrative privileges...
-    runas /user:Administrator "%~f0"
-    pause
-    exit /b
-)
 
 cmd /c "cd src\server\translation_module && cargo build && maturin build && pip install target\wheels\translation_module-0.1.0*.whl"
-cmd /c "cd src\server\core && python admin.py"
-cmd /c "cd src\client && npm install react@18 react-dom@18 && npm run build && npm start"
+cmd /k "cd src\server && python manage.py runserver"
+cmd /k "cd src\client && npm install react@18 react-dom@18 && npm run build && npm start"
 
 echo ✅ Started
 pause
--- src/server/translation_app/urls.py
+++ src/server/translation_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.find_language, name="find language"),
-    path("", views.translate, name="translate"),
+    path("find_language", views.find_language, name="find language"),
+    path("translate", views.translate, name="translate"),
 ]
