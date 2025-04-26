f81ca4fcaf4b4f6285f1a3c9c4512ae0ec7ab239
SzymonKamiński <szymon.kaminkk@gmail.com>
2025-04-26 20:54:12 +0200

    feat: no text printing while building fix: now frontend and backend servers deploy corectly

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 @echo off
-echo 🚀 Starting server...
+echo 🚀 Starting app...
 
-
-cmd /c "cd src\server\translation_module && cargo build && maturin build && pip install target\wheels\translation_module-0.1.0*.whl"
-cmd /k "cd src\server && python manage.py runserver"
-cmd /k "cd src\client && npm install react@18 react-dom@18 && npm run build && npm start"
+echo Bulding...
+cmd /c "cd src\server\translation_module && cargo build > NUL 2>&1 && maturin build > NUL 2>&1 && pip install target\wheels\translation_module-0.1.0*.whl > NUL 2>&1"
+echo Starting servers...
+start "Backend server" cmd /k "cd src\server && python manage.py runserver"
+start "Frontend server" cmd /k "cd src\client && npm install react@18 react-dom@18 && npm run build && npm start"
 
 echo ✅ Started
 pause
