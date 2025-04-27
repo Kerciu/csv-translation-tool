3620e5f1a5bd78610859aaf28c502b0b01c66983
SzymonKamiński <szymon.kaminkk@gmail.com>
2025-04-27 11:45:08 +0200

    quickfix: now test_db_connection works properly

--- src/server/auth_app/urls.py
+++ src/server/auth_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("log", views.log_in, name="log_in"),
     path("sign", views.sign_up, name="sign_up"),
-    path("test_db_connection", views.sign_up, name="test"),
+    path("test_db_connection", views.test_db_connection, name="test"),
 ]
--- src/server/auth_app/views.py
+++ src/server/auth_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Viewer
+from django.http import JsonResponse
 
 def log_in(request):
     return HttpResponse("Logged in")
@@ -9,6 +10,6 @@ def sign_up(request):
     return HttpResponse("Signed up")
 
 def test_db_connection(request):
-    viewers = Viewer.objects.order_by("name")[:10]
-    return render(request, "test", {"viewers": viewers})
+    viewers = Viewer.objects.order_by("name").values_list("name", flat=True)[:10]
+    return JsonResponse(list(viewers), safe=False)
 
