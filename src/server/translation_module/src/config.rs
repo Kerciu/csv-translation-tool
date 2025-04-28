90567214e751d1daf37c59bec624dd219d5d9b8b
Kerciu <kerciuuu@gmail.com>
2025-04-28 19:57:17 +0200

    loading repo and checking if model exists, returning valid model configuration

--- src/server/translation_module/src/config.rs
+++ src/server/translation_module/src/config.rs
@@ -21,4 +21,17 @@ pub fn validate_language(lang: &str) -> Result<()> {
 pub fn get_model_config(src_lang: &str, tgt_lang: &str) -> Result<ModelConfig> {
     validate_language(&src_lang)?;
     validate_language(&tgt_lang)?;
+
+    let model_repo = Repo::new(format!("Helsinki-NLP/opus-mt-{}-{}", src_lang, tgt_lang));
+    let api = Api::new()?;
+
+    if api.model(model_repo.clone().id).get("model.safetensors").is_err() {
+        return Err(Error::msg(format!("No model available for {}-{} language pair", src_lang, tgt_lang)));
+    }
+
+    Ok(ModelConfig {
+        model_repo,
+        src_token: format!(">>{}<<", src_lang),
+        src_token: format!(">>{}<<", src_lang)
+    })
 }
