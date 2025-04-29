2913101b6c94719d7aa998be8f3a49654b476019
Kerciu <kerciuuu@gmail.com>
2025-04-30 00:26:18 +0200

    changed package manager to pnpm

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -27,6 +27,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  output: process.env.NEXT_OUTPUT === 'production' ? 'standalone' : undefined,
 }
 
 if (userConfig) {
