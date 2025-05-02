3cb8917ab68afdab530c9c58ed72fe877b57bb26
Kerciu <kerciuuu@gmail.com>
2025-05-02 14:52:22 +0200

    changed fields of model config to resemble the actual marianmt config fields, removed unused imports

--- model/src/config.rs
+++ model/src/config.rs
@@ -1,6 +1,5 @@
 use anyhow::{Result, Error};
 use hf_hub::api::sync::Api;
-use std::path::PathBuf;
 use std::process::Command;
 use candle_transformers::models::marian;
 
@@ -9,7 +8,10 @@ pub struct ModelConfig {
     pub model_id: String,
     pub src_token: String,
     pub tgt_token: String,
+    pub decoder_start_token_id: u32,
+    pub max_position_embeddings: usize,
     pub eos_token_id: u32,
+    pub forced_eos_token_id: u32,
 }
 
 impl ModelConfig {
@@ -76,15 +78,19 @@ pub fn get_model_config(src_lang: &str, tgt_lang: &str) -> Result<ModelConfig> {
     let model_id = build_model_id(src_lang, tgt_lang);
     check_model_exists(&model_id)?;
 
-    // for simplicity, assuming the eos_token_id is defined for each language pair
-    // will likely need to adjust this based on how we fetch it from the model
-    let eos_token_id = 1; // will need to replace with the correct value for your tokenizer
-
+    let marian_config = match model_id.as_str() {
+        "Helsinki-NLP/opus-mt-tc-big-fr-en" => marian::Config::opus_mt_tc_big_fr_en(),
+        // Add other model configs here
+        _ => return Err(Error::msg("Unsupported model")),
+    };
 
     Ok(ModelConfig {
         model_id,
         src_token: format!(">>{}<<", src_lang),
         tgt_token: format!(">>{}<<", tgt_lang),
-        eos_token_id
+        decoder_start_token_id: marian_config.decoder_start_token_id,
+        max_position_embeddings: marian_config.max_position_embeddings,
+        eos_token_id: marian_config.eos_token_id,
+        forced_eos_token_id: marian_config.forced_eos_token_id,
     })
 }
