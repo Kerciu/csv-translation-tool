8d72b410d9958af52c4f01376ae153329c64dc85
Kerciu <kerciuuu@gmail.com>
2025-05-02 14:51:57 +0200

    added allow unsafe operation in unsafe function attribute

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -6,6 +6,7 @@ use model::TranslationModel;
 use pyo3::prelude::*;
 
 #[pyfunction]
+#[allow(unsafe_op_in_unsafe_fn)]
 fn translate(text: &str, src_lang: &str, tgt_lang: &str) -> PyResult<String> {
     let config = get_model_config(&src_lang, &tgt_lang)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
