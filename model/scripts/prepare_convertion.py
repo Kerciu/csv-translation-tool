c418bceeafd9490c79e8bf4e38ce2c1bcf16bd2a
Kerciu <kerciuuu@gmail.com>
2025-05-04 15:10:34 +0200

    feat: combined all the preparation scripts into one prepare convertion python script

---
+++ model/scripts/prepare_convertion.py
@@ -0,0 +1,31 @@
+from convert_pt_to_safetensors import convert_pt_to_safetensors
+from convert_slow_tokenizer import convert_slow_tokenizer
+from download_pytorch_model import download_model
+import sys
+import os
+
+if __name__ == "__main__":
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    if len(sys.argv) != 3:
+        print("Usage: python prepare_convertion.py <src_lang> <tgt_lang>")
+        sys.exit(1)
+
+    src_lang = sys.argv[1]
+    tgt_lang = sys.argv[2]
+
+    models_dir = os.path.join(script_dir, "models")
+
+    download_model(
+        model_name=f"{src_lang}-{tgt_lang}",
+        save_path=os.path.join(models_dir, "pytorch_model.bin")
+    )
+    convert_slow_tokenizer(src_lang=src_lang, tgt_lang=tgt_lang, dest_folder=models_dir)
+    convert_pt_to_safetensors(
+        source_folder=models_dir,
+        dest_folder=models_dir,
+        delete_old=False
+    )
+
+    print("Conversion completed successfully.")
