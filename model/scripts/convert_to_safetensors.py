70c88630ba89a0d4f5c913019d1b4791d476a52b
Kerciu <kerciuuu@gmail.com>
2025-05-04 12:53:51 +0200

    rewritten script to convert to safetensor format

--- model/scripts/convert_to_safetensors.py
+++ model/scripts/convert_to_safetensors.py
@@ -1,31 +1,17 @@
 from transformers import AutoModelForSeq2SeqLM
-from safetensors.torch import save_file
-import torch
-import os
+from convert_tokenizer import convert_marian_tokenizer
 import sys
+import os
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python convert_to_safetensor.py <model_id> <output_dir>")
-        sys.exit(1)
-
     model_id = sys.argv[1]
     output_dir = sys.argv[2]
-    os.makedirs(output_dir, exist_ok=True)
 
-    print(f"Loading model {model_id}...")
     model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+    model.save_pretrained(output_dir, safe_serialization=True)
 
-    print("Saving PyTorch model in Hugging Face format...")
-    model.save_pretrained(output_dir)
-
-    print("Converting to safetensors...")
-    tensors = {k: v.cpu() for k, v in model.state_dict().items()}
-    safetensors_path = os.path.join(output_dir, "model.safetensors")
-    save_file(tensors, safetensors_path)
-
-    print(f"Model converted to safetensors and saved in {safetensors_path}")
+    convert_marian_tokenizer(model_id, output_dir)
 
 
 if __name__ == "__main__":
