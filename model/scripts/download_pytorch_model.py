544e1b1273e59891eb7ca534a87f425fc4481524
Kerciu <kerciuuu@gmail.com>
2025-05-04 15:21:11 +0200

    downloading config.json in addition to pytorch binary model

--- model/scripts/download_pytorch_model.py
+++ model/scripts/download_pytorch_model.py
@@ -6,17 +6,23 @@ def build_model_name(model_name):
     return f"Helsinki-NLP/opus-mt-{model_name}"
 
 
-def download_model(model_name, save_path):
-
-    response = requests.get(f"https://huggingface.co/{build_model_name(model_name)}/resolve/main/pytorch_model.bin")
+def download_file(url, save_path):
+    response = requests.get(url)
 
     if response.status_code == 200:
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, 'wb') as f:
             f.write(response.content)
-
-        print(f"Model downloaded and saved to {save_path}")
+        print(f"Downloaded and saved to {save_path}")
     else:
-        print(f"Failed to download model. Status code: {response.status_code}")
-        print("Please check the model name and try again.")
-        return None
+        print(f"Failed to download from {url}. Status code: {response.status_code}")
+
+
+def download_model(model_name, save_dir):
+    base_url = f"https://huggingface.co/{build_model_name(model_name)}/resolve/main"
+
+    model_path = os.path.join(save_dir, "pytorch_model.bin")
+    download_file(f"{base_url}/pytorch_model.bin", model_path)
+
+    config_path = os.path.join(save_dir, "config.json")
+    download_file(f"{base_url}/config.json", config_path)
