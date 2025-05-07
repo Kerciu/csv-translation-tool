a7f7b76db6c03e2f6b81d0268db7dfd36b104308
Kerciu <kerciuuu@gmail.com>
2025-05-07 19:13:32 +0200

    created healthcheck endpoint in frontend and backend for docker-compose

--- server/auth_app/urls.py
+++ server/auth_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('health', views.health_check, name='health_check'),
     path("log", views.log_in, name="log_in"),
     path("sign", views.sign_up, name="sign_up"),
     path("test_db_connection", views.test_db_connection, name="test"),
