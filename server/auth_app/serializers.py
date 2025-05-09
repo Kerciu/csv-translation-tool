7d929e48002ba9c2c4479aa5335fce464e854950
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-10 01:28:22 +0200

    feat: added UserSignUp validation

--- server/auth_app/serializers.py
+++ server/auth_app/serializers.py
@@ -2,21 +2,35 @@ from rest_framework import serializers
 from .models import CustomUser
 from datetime import datetime
 import os
-
+import argon2
 
 class UserSignUpSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
+    password = serializers.CharField(read_only=True)
 
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'password']
 
+    
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        if not "@" in email:
+            raise serializers.ValidationError("Invalid email")
+        if CustomUser.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Email already occupied")
+        
+        username = attrs.get('username', '')
+        if CustomUser.objects.filter(username=username).exists():
+            raise serializers.ValidationError("Username occupied")
+
+        return attrs
 
     def create(self, validated_data):
         username = validated_data['username']
         email = validated_data['email']
-        salt = "salt"
-        password = validated_data['password']
+        salt = os.urandom(32)
+        password = argon2.hash_password(salt+validated_data['password'])
         date_joined = datetime.now()
 
         user = CustomUser.objects.create(
