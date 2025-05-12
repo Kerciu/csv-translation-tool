f767c5ff18d16a8690a54a76fbb6fd41afe57ab2
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-12 22:09:31 +0200

    feat: added SignUpSerializer tests

--- server/auth_app/tests/test_auth_serializers.py
+++ server/auth_app/tests/test_auth_serializers.py
@@ -1,7 +1,57 @@
-from ..serializers import UserSerializer
+from ..serializers import UserSerializer, UserSignUpSerializer
 from ..models import CustomUser
 from datetime import datetime
 from django.test import RequestFactory, TestCase
+
+
+class UserSignUpSerializerTest(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="taken",
+            email="taken@taken.com",
+            password="pass",
+            date_joined=datetime.now()
+        )
+         
+    def test_valid_registration(self):
+        data = {
+            "username": "newuser",
+            "email": "new@new.com",
+            "password": "pass",
+        }
+        serializer = UserSignUpSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertEqual(user.username, "newuser")
+        self.assertEqual(user.email, "new@new.com")
+    
+    def test_email_taken(self):
+        data = {
+            "username": "newuser",
+            "email": "taken@taken.com",
+            "password": "pass",
+        }
+        serializer = UserSignUpSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            str(serializer.errors['email'][0]), "custom user with this email already exists."
+        )
+
+    def test_username_taken(self):
+        data = {
+            "username": "taken",
+            "email": "new@new.com",
+            "password": "pass",
+        }
+        serializer = UserSignUpSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            str(serializer.errors['username'][0]), "custom user with this username already exists."
+        )
+
+
+
 
 class UserSerializerTest(TestCase):
     
@@ -16,7 +66,7 @@ class UserSerializerTest(TestCase):
     def test_valid_user(self):
         user_serializer = UserSerializer(self.user)
         user_data = user_serializer.data
-        self.assertTrue(user_data['username'], 'user')
-        self.assertTrue(user_data['email'], 'user@user.com')
+        self.assertEqual(user_data['username'], 'user')
+        self.assertEqual(user_data['email'], 'user@user.com')
 
 
