626eb32c350b7c90859481f5b3f7d84031866139
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-05-25 13:23:59 +0200

    feat: added returing detected language

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod translation;
 pub mod config;
-use anyhow::Ok;
 use clap::builder::Str;
 use config::get_model_config;
 use translation::{ model::TranslationModel};
@@ -9,7 +8,7 @@ use crate::translation::detect_language::detect_language;
 
 #[pyfunction]
 #[allow(unsafe_op_in_unsafe_fn)]
-fn translate(text: &str, src_lang: &str, tgt_lang: &str) -> PyResult<String> {
+fn translate(text: &str, src_lang: &str, tgt_lang: &str) -> PyResult<(String, String)> {
     let detected_lang = if src_lang == "any" {
             match detect_language(text) {
                 Some(lang) => lang.iso_code_639_1().to_string(),
@@ -18,17 +17,23 @@ fn translate(text: &str, src_lang: &str, tgt_lang: &str) -> PyResult<String> {
         } else {
             src_lang.to_string()
         };
+
     if detected_lang == "None"{
-        Ok("Could not detect language.".to_string());
+        return Ok(("Could not detect language.".to_string(), "None".to_string()));
     }
-    let config = get_model_config(&src_lang, &tgt_lang)
+
+    let det_lang = &detected_lang;
+
+    let config = get_model_config(det_lang, &tgt_lang)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
 
     let mut model = TranslationModel::new(config)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
 
-    model.translate(text)
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
+    let translated_text= model.translate(text)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
+
+    Ok((translated_text, detected_lang))
 }
 
 #[pymodule]
