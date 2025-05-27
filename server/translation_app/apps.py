44a535a188e0de96b6d19f361035c5e6a9597ae0
Kerciu <kerciuuu@gmail.com>
2025-05-27 17:03:58 +0200

    feat: initialize translator in TranslationAppConfig for warmup on startup

--- server/translation_app/apps.py
+++ server/translation_app/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class TranslationAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "translation_app"
+
+    def ready(self):
+        from .rust_loader import get_translator
+        translator = get_translator()
+
+        # warmup the translator
+        dummy_batch = ["hello"] * 8
+        translator.translate_batch("es", dummy_batch)
+        translator.translate_batch("en", dummy_batch)
