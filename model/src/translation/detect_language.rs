8089aaa9f67d8911cf5261c5a33a1c7a9b3a1c88
Kerciu <Kerciuuu@gmail.com>
2025-06-03 13:54:32 +0200

    feat: improved sensitivity of lanugage detection

--- model/src/translation/detect_language.rs
+++ model/src/translation/detect_language.rs
@@ -5,6 +5,10 @@ pub use lingua::Language::{
 use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
 
 pub fn detect_language(text: &str) -> Option<Language> {
+    if text.trim().len() < 5 {
+        return None;
+    }
+
     let languages = vec![
         English, French, German, Spanish, Korean, Japanese, Italian, Portuguese, Russian, Chinese,
         Arabic, Hindi, Indonesian, Polish, Dutch, Swedish, Thai, Turkish, Vietnamese,
@@ -12,7 +16,16 @@ pub fn detect_language(text: &str) -> Option<Language> {
 
     let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
 
-    detector.detect_language_of(text)
+    let mut results = detector.compute_language_confidence_values(text);
+    results.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    let (language, confidence) = results.first()?;
+
+    if *confidence < 0.20 {
+        return None;
+    }
+
+    Some(*language)
 }
 
 pub fn map_language_to_code(language: Language) -> String {
@@ -36,7 +49,5 @@ pub fn map_language_to_code(language: Language) -> String {
         Thai => "th".to_string(),
         Turkish => "tr".to_string(),
         Vietnamese => "vi".to_string(),
-
-        _ => "unknown".to_string(),
     }
 }
